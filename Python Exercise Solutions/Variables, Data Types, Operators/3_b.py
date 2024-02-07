0d85f21cd2e7e5664b1e011b527552d113bb24cb
Sagar Sen <sagarsen2023@gmail.com>
2024-02-07 23:31:11 +0530

    Create 3_b.py

---
+++ Python Exercise Solutions/Variables, Data Types, Operators/3_b.py
@@ -0,0 +1,7 @@
+num = int(input("Entyer the number to check: "))
+if num == 0:
+  print("The number is zero.\n")
+elif (num%2 == 0):
+  print("The number is even.\n")
+else:
+  print("The number is odd.")
