9bb826f703c9f201bef8bb1b308372010771f6dc
Sagar Sen <sagarsen2023@gmail.com>
2024-02-07 23:39:53 +0530

    Create 4_a.py

---
+++ Python Exercise Solutions/Conditional Statements, Loops/4_a.py
@@ -0,0 +1,8 @@
+# 4_a.py
+num = int(input("Enter the number to check: "))
+if(num > 0):
+  print("The number is a positive number.")
+elif num < 0:
+  print("The number is a negetive number")
+else:
+  print("You have entered zero")
